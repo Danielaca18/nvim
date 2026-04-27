35dbf31357350dd9c5741d5085d69ac26cdb36c0
Daniel Castro <dcastro@ualberta.ca>
2026-04-26 19:46:17 -0600

    updated clipboard logic

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+if os.getenv("SSH_CONNECTION") then
+  -- Use OSC 52 for SSH sessions
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+else
+  -- Use native Wayland/X11 tools for local sessions
+  -- Neovim will automatically pick wl-copy if it's in your PATH
+  vim.g.clipboard = nil
+end
 
 vim.opt.clipboard = "unnamedplus"
